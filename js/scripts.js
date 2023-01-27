f0a17d5524b154b4712736623f20a1b429120b18
John Lenz <120522526+jlenz27@users.noreply.github.com>
2023-01-27 09:26:06 -0800

    it will add the object of a pizza order

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Buisness Logic--
+
+function PizzaOrder(pizzaSize, pizzaTopping, pizzaToppingTwo){
+    this.pizzaSize = pizzaSize;
+    this.pizzaTopping = pizzaTopping;
+    this.pizzaToppingTwo = pizzaToppingTwo;
+}
+
